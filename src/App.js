3617cddcf2369f0211856d1ed301ee656b643670
unknown <busrabireel123@gmail.com>
2025-04-13 11:56:43 +0300

    giris yazisi degistirildi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Github Kursuna Hoşgeldiniz!
+          Github Öğreniyorum Kursuna Hoşgeldiniz!
         </p>
         <a
           className="App-link"
